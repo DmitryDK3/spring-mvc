2010acb89779dc37ae5b273d2ffa9a11bf8c7b62
DmitryDK3 <dmitrydk3@gmail.com>
2021-05-12 18:36:50 +0300

    request param

--- spring_mvc/src/main/java/web/controller/CarController.java
+++ spring_mvc/src/main/java/web/controller/CarController.java
@@ -3,15 +3,14 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import web.Service.CarService;
-import javax.servlet.ServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.serv.*;
 
 @Controller
 public class CarController {
-
     @GetMapping(value = "/cars")
-    public String getCars(ModelMap model, ServletRequest servletRequest) {
-        model.addAttribute("cars", CarService.getSomeCars(servletRequest));
+    public static String getCars(@RequestParam(defaultValue = "6") int count, ModelMap model) {
+        model.addAttribute("cars", CarService.getSomeCars(count));
         return "cars";
     }
 }
